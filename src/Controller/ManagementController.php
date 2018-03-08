29eeed8432da3de8c1ef024292e1f519ee0499e0
CORI <greengirls-18@hotmail.fr>
2018-03-08 11:53:11 +0100

    ajout dans le detail du site ses relevés

--- src/Controller/ManagementController.php
+++ src/Controller/ManagementController.php
@@ -8,104 +8,98 @@ use Cake\I18n\Time;
 class ManagementController extends AppController {
 
     public function accueil() {
-        $counter=0;
-                
+        $counter = 0;
+
         //connexion
         $this->loadModel("Users");
-        $m=$this->Users->find();
-        
-         if($this->request->is("post")){
-            $login=$this->request->data["login"];
-            $password=$this->request->data["password"];
-            
-            foreach ($m as $user){
-                
-                if($user->login==$login AND $user->passwd==$password) {                   
-                    $counter=1;
+        $m = $this->Users->find();
+
+        if ($this->request->is("post")) {
+            $login = $this->request->data["login"];
+            $password = $this->request->data["password"];
+
+            foreach ($m as $user) {
+
+                if ($user->login == $login AND $user->passwd == $password) {
+                    $counter = 1;
                 }
             }
-            if ($counter==0) echo '<script language="Javascript"> alert ("vous êtes pas encore inscrit" )</script>';
+            if ($counter == 0)
+                echo '<script language="Javascript"> alert ("vous êtes pas encore inscrit" )</script>';
             else {
                 session_start();
                 $_SESSION['login'] = $login;
                 echo '<script language="Javascript"> alert ("vous êtes maintenant connecté !" )</script>';
-            }  
-         }   
-         
+            }
+        }
     }
-    
+
     public function deconnexion() {
-          // Démarrage ou restauration de la session
-  session_start();
-  // Réinitialisation du tableau de session
-  // On le vide intégralement
-  $_SESSION = array();
-  // Destruction de la session
-  session_destroy();
-  // Destruction du tableau de session
-  unset($_SESSION);
-  
-  echo "<script> alert('Vous êtes déconnecté !');window.location.href='http://localhost/nrjsite/management/accueil';</script>";
+        // Démarrage ou restauration de la session
+        session_start();
+        // Réinitialisation du tableau de session
+        // On le vide intégralement
+        $_SESSION = array();
+        // Destruction de la session
+        session_destroy();
+        // Destruction du tableau de session
+        unset($_SESSION);
+
+        echo "<script> alert('Vous êtes déconnecté !');window.location.href='http://localhost/nrjsite/management/accueil';</script>";
     }
-    
 
     public function inscription() {
         $this->loadModel("Users");
         $new = $this->Users->newEntity();
-        
-         if($this->request->is("post")){
-            $new->login=$this->request->data["login"];
-            $new->passwd=$this->request->data["password"];
-            $checkpasswd=$this->request->data["password_checking"];
-            
-            if($new->passwd==$checkpasswd)
-            $this->Users->save($new);
-            
-            else echo '<script language="Javascript"> alert ("password non identique" )</script>';
+
+        if ($this->request->is("post")) {
+            $new->login = $this->request->data["login"];
+            $new->passwd = $this->request->data["password"];
+            $checkpasswd = $this->request->data["password_checking"];
+
+            if ($new->passwd == $checkpasswd)
+                $this->Users->save($new);
+            else
+                echo '<script language="Javascript"> alert ("password non identique" )</script>';
         }
-       $this->set("new",$new);
+        $this->set("new", $new);
     }
 
     public function listeSites() {
         //test user connecté
         session_start();
         // On teste si la variable de session existe et contient une valeur
-        if(empty($_SESSION['login'])) 
-        {
+        if (empty($_SESSION['login'])) {
             //si l'user n'est pas connecté on redirige vers l'accueil
             echo "<script> alert('Page sécurisée, il faut vous connecter !');window.location.href='http://localhost/nrjsite/management/accueil';</script>";
-                         
-        }
-        else {
-        
-        $this->loadModel('Sites');
-      
-        //trouver la liste des sites existants pour l'afficher
-        $m=$this->Sites->find();
-        $this->set("m",$m);
-        
-        //formulaire pour ajouter un nouveau site
-        $new = $this->Sites->newEntity();
-        if ($this->request->is('post')) {
-                         
-                if(($this->request->data['type'] != 'producer' AND
-                $this->request->data['type'] != 'consumer')|| 
-                ($this->request->data['stock'] <0) || 
-                ($this->request->data['location_x'] <-90) || ($this->request->data['location_x'] >90) || ($this->request->data['location_y'] <-180) || ($this->request->data['location_x'] >180))
-                
+        } else {
+
+            $this->loadModel('Sites');
+
+            //trouver la liste des sites existants pour l'afficher
+            $m = $this->Sites->find();
+            $this->set("m", $m);
+
+            //formulaire pour ajouter un nouveau site
+            $new = $this->Sites->newEntity();
+            if ($this->request->is('post')) {
+
+                if (($this->request->data['type'] != 'producer' AND
+                        $this->request->data['type'] != 'consumer') ||
+                        ($this->request->data['stock'] < 0) ||
+                        ($this->request->data['location_x'] < -90) || ($this->request->data['location_x'] > 90) || ($this->request->data['location_y'] < -180) || ($this->request->data['location_x'] > 180))
                     echo '<script language="Javascript"> alert ("saisie non valide" )</script>';
-           
-            else{
-            $new->name=$this->request->data['name'];
-            $new->type=$this->request->data['type'];
-            $new->location_x=$this->request->data['location_x'];
-            $new->location_y=$this->request->data['location_y'];
-            $new->stock=$this->request->data['stock'];
-            $this->Sites->save($new);   
+
+                else {
+                    $new->name = $this->request->data['name'];
+                    $new->type = $this->request->data['type'];
+                    $new->location_x = $this->request->data['location_x'];
+                    $new->location_y = $this->request->data['location_y'];
+                    $new->stock = $this->request->data['stock'];
+                    $this->Sites->save($new);
+                }
             }
-            
-        }
-        $this->set('new', $new);
+            $this->set('new', $new);
         }
     }
 
@@ -113,162 +107,163 @@ class ManagementController extends AppController {
         //test user connecté
         session_start();
         // On teste si la variable de session existe et contient une valeur
-        if(empty($_SESSION['login'])) 
-        {
+        if (empty($_SESSION['login'])) {
             //si l'user n'est pas connecté on redirige vers l'accueil
             echo "<script> alert('Page sécurisée, il faut vous connecter !');window.location.href='http://localhost/nrjsite/management/accueil';</script>";
-                         
-        }
-        else {
-       
-        $this->loadModel('Sites');
-        $this->loadModel('Paths');
-       
-        $new = $this->Sites->get($idsite);
-        
-        //trouver les sites existants
-        $m=$this->Sites->find();
-        
-        //trier les sites producer si le site actuel est consumer et vice-versa 
-        $tabSitesTries=array();
-        
-        foreach($m as $site){
-            if($site->type!=$new->type) $tabSitesTries[]=$site->name;
-        }
-        $this->set("tabSitesTries",$tabSitesTries);
-        
-        //formulaire nouvelle voie
-        $newPath = $this->Paths->newEntity();
-        $sitename="";
-        
-        if ($this->request->is('post') && $this->request->getData('submit') == 'Ajouter la voie' ){
-            $sitename=$tabSitesTries[$this->request->data['SiteName']];  
-  
-         foreach($m as $site){
-            if($site->name==$sitename) {
-                $newPath->ending_site_id=$site->id;
-                $newPath->starting_site_id=$new->id;
-                $newPath->max_capacity=$this->request->data['max_capacity'];
-                $newPath->name=$this->request->data['name'];
-                $this->Paths->save($newPath);
+        } else {
+
+            $this->loadModel('Sites');
+            $this->loadModel('Paths');
+
+            $new = $this->Sites->get($idsite);
+
+            //trouver les sites existants
+            $m = $this->Sites->find();
+
+            //trier les sites producer si le site actuel est consumer et vice-versa 
+            $tabSitesTries = array();
+
+            foreach ($m as $site) {
+                if ($site->type != $new->type)
+                    $tabSitesTries[] = $site->name;
             }
-        }
+            $this->set("tabSitesTries", $tabSitesTries);
+
+            //formulaire nouvelle voie
+            $newPath = $this->Paths->newEntity();
+            $sitename = "";
+
+            if ($this->request->is('post') && $this->request->getData('submit') == 'Ajouter la voie') {
+                $sitename = $tabSitesTries[$this->request->data['SiteName']];
+
+                foreach ($m as $site) {
+                    if ($site->name == $sitename) {
+                        $newPath->ending_site_id = $site->id;
+                        $newPath->starting_site_id = $new->id;
+                        $newPath->max_capacity = $this->request->data['max_capacity'];
+                        $newPath->name = $this->request->data['name'];
+                        $this->Paths->save($newPath);
+                    }
+                }
             }
-            
-            //formulaire nouveau relevé
+
+
+            //liste des relevés du site
             $this->loadModel('Records');
-            $newRecords=$this->Records->newEntity();
-            $now=Time::now();
-            $now->timezone='Europe/Paris';
-            if ($this->request->is('post') && $this->request->getData('submit') == 'Ajouter le relevé' ){
-                $newRecords->site_id=$idsite;
-                $newRecords->date=$now;
-                $newRecords->value=$this->request->data['value'];
-                $this->Records->save($newRecords);
-            }            
-        
-        
-        //formulaire edition
-        if ($this->request->is(['post', 'put'])&& $this->request->getData('submit') == 'Editer') {
-            
-            if(($this->request->data['type'] != 'producer' AND
-                $this->request->data['type'] != 'consumer')|| 
-                ($this->request->data['stock'] <0) || 
-                ($this->request->data['location_x'] <-90) || ($this->request->data['location_x'] >90) || ($this->request->data['location_y'] <-180) || ($this->request->data['location_x'] >180))
-                
-                    echo '<script language="Javascript"> alert ("saisie non valide" )</script>';
-           
-            else{
-            $new->name=$this->request->data['name'];
-            $new->type=$this->request->data['type'];
-            $new->location_x=$this->request->data['location_x'];
-            $new->location_y=$this->request->data['location_y'];
-            $new->stock=$this->request->data['stock'];
-            $this->Sites->save($new);   
-        }  
-    }
-    
-    $this->set('new', $new);
-        }
+            $listeRecords = $this->Records->find();
+
+            $listeRecordsDuSite = array();
+
+            foreach ($listeRecords as $record) {
+
+                if ($record->site_id == $idsite)
+                    $listeRecordsDuSite[] = $record;
             }
+            $this->set("listeRecordsDuSite", $listeRecordsDuSite);
+
+            //formulaire nouveau relevé
+            $newRecords = $this->Records->newEntity();
+            $now = Time::now();
+            $now->timezone = 'Europe/Paris';
+            if ($this->request->is('post') && $this->request->getData('submit') == 'Ajouter le relevé') {
+                $newRecords->site_id = $idsite;
+                $newRecords->date = $now;
+                $newRecords->value = $this->request->data['value'];
+                $this->Records->save($newRecords);
+            }
+
+
+            //formulaire edition
+            if ($this->request->is(['post', 'put']) && $this->request->getData('submit') == 'Editer') {
+
+                if (($this->request->data['type'] != 'producer' AND
+                        $this->request->data['type'] != 'consumer') ||
+                        ($this->request->data['stock'] < 0) ||
+                        ($this->request->data['location_x'] < -90) || ($this->request->data['location_x'] > 90) || ($this->request->data['location_y'] < -180) || ($this->request->data['location_x'] > 180))
+                    echo '<script language="Javascript"> alert ("saisie non valide" )</script>';
+
+                else {
+                    $new->name = $this->request->data['name'];
+                    $new->type = $this->request->data['type'];
+                    $new->location_x = $this->request->data['location_x'];
+                    $new->location_y = $this->request->data['location_y'];
+                    $new->stock = $this->request->data['stock'];
+                    $this->Sites->save($new);
+                }
+            }
+
+            $this->set('new', $new);
+        }
+    }
 
     public function listeVoies() {
         //test user connecté
         session_start();
         // On teste si la variable de session existe et contient une valeur
-        if(empty($_SESSION['login'])) 
-        {
+        if (empty($_SESSION['login'])) {
             //si l'user n'est pas connecté on redirige vers l'accueil
             echo "<script> alert('Page sécurisée, il faut vous connecter !');window.location.href='http://localhost/nrjsite/management/accueil';</script>";
-                         
-        }
-        else {
-        $this->loadModel('Paths');
-        $this->loadModel('Sites');
-               
-        //trouver les voies existantes
-        $p=$this->Paths->find();
-        $this->set("p",$p);
-        
-        //trouver les sites existants
-        $m=$this->Sites->find();
-        $this->set("m",$m);
-        
-    }}
-    
-    public function deleteSite($id){
-        
-       $this->loadModel('Sites');
-       $entity = $this->Sites->get($id);
-       $result = $this->Sites->delete($entity);
+        } else {
+            $this->loadModel('Paths');
+            $this->loadModel('Sites');
 
+            //trouver les voies existantes
+            $p = $this->Paths->find();
+            $this->set("p", $p);
+
+            //trouver les sites existants
+            $m = $this->Sites->find();
+            $this->set("m", $m);
+        }
     }
-    
-     public function carte() {
-         //test user connecté
+
+    public function deleteSite($id) {
+
+        $this->loadModel('Sites');
+        $entity = $this->Sites->get($id);
+        $result = $this->Sites->delete($entity);
+    }
+
+    public function carte() {
+        //test user connecté
         session_start();
         // On teste si la variable de session existe et contient une valeur
-        if(empty($_SESSION['login'])) 
-        {
+        if (empty($_SESSION['login'])) {
             //si l'user n'est pas connecté on redirige vers l'accueil
             echo "<script> alert('Page sécurisée, il faut vous connecter !');window.location.href='http://localhost/nrjsite/management/accueil';</script>";
-                         
-        }
-        else {
-        $this->loadModel('Sites');
-      
-        //trouver la liste des sites existants pour l'afficher
-        $m=$this->Sites->find();
-        $this->set("m",$m);
-       
+        } else {
+            $this->loadModel('Sites');
+
+            //trouver la liste des sites existants pour l'afficher
+            $m = $this->Sites->find();
+            $this->set("m", $m);
+
 // Convertir la requête en tableau va l'exécuter.
-$tab = $m->toArray();
+            $tab = $m->toArray();
 
- $this->set("tab",$tab);
- 
- $locx=array(); //tableau des latitudes des sites
- $locy=array(); //tableau des longitudes
- $type=array(); //tableau des types (consumer ou producer)
- $name=array(); //tableau des noms
- $stock=array(); //tableau des stocks
- 
- //remplissage des tableaux 
- for ($iter = 0; $iter < sizeof($tab); $iter++) {
-        
- $locx[]=$tab[$iter]->location_x;
- $locy[]=$tab[$iter]->location_y;
- 
- $type[]=$tab[$iter]->type;
- $name[]=$tab[$iter]->name;
- $stock[]=$tab[$iter]->stock;
-      
+            $this->set("tab", $tab);
+
+            $locx = array(); //tableau des latitudes des sites
+            $locy = array(); //tableau des longitudes
+            $type = array(); //tableau des types (consumer ou producer)
+            $name = array(); //tableau des noms
+            $stock = array(); //tableau des stocks
+            //remplissage des tableaux 
+            for ($iter = 0; $iter < sizeof($tab); $iter++) {
+
+                $locx[] = $tab[$iter]->location_x;
+                $locy[] = $tab[$iter]->location_y;
+
+                $type[] = $tab[$iter]->type;
+                $name[] = $tab[$iter]->name;
+                $stock[] = $tab[$iter]->stock;
+            }
+            $this->set("locx", $locx);
+            $this->set("locy", $locy);
+            $this->set("type", $type);
+            $this->set("name", $name);
+            $this->set("stock", $stock);
+        }
     }
-    $this->set("locx", $locx);
-     $this->set("locy", $locy);
-     $this->set("type", $type);
-      $this->set("name", $name);
-      $this->set("stock", $stock);
-}
-}
- }
 
+}
--- src/Template/Management/details_site.ctp
+++ src/Template/Management/details_site.ctp
@@ -1,48 +1,63 @@
 <?= $this->Html->css('test.css') ?>
 
 <ul id="detailsSite">
-<?php 
-echo "<li>Nom : ".$new->name."</li>";
-echo "<li>Type : ".$new->type."</li>";
-echo "<li>Latitude : ".$new->location_x."</li>";
-echo "<li>Longitude : ".$new->location_y."</li>";
-echo "<li>Stock : ".$new->stock."</li>";
-?>
+    <?php
+    echo "<li>Nom : " . $new->name . "</li>";
+    echo "<li>Type : " . $new->type . "</li>";
+    echo "<li>Latitude : " . $new->location_x . "</li>";
+    echo "<li>Longitude : " . $new->location_y . "</li>";
+    echo "<li>Stock : " . $new->stock . "</li>";
+    ?>
 </ul>
+
+<!--Relevés du site-->
+<table>
+    <h4>Relevés</h4>
+    <?php
+    foreach ($listeRecordsDuSite as $record) {
+        echo"<tr>";
+        echo"<td>Date : " . $record->date . "</td>";
+        echo"<td>Valeur : " . $record->value . "</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
 
 <!--ajouter un relevé-->
 <section class="large-6">
     <h4>Ajouter un relevé au site</h4>
-<?= $this->Form->create() ?>
-<?= $this->Form->input('value', array('label'=>'Valeur','type'=>'number'))?>
-<?= $this->Form->submit('Ajouter le relevé',array('name' => 'submit')) ?>
-<?= $this->Form->end() ?>
+    <?= $this->Form->create() ?>
+    <?= $this->Form->input('value', array('label' => 'Valeur', 'type' => 'number')) ?>
+    <?= $this->Form->submit('Ajouter le relevé', array('name' => 'submit')) ?>
+    <?= $this->Form->end() ?>
 </section>
 
 <!--ajouter voies-->
 <section class="large-6">
     <h4>Ajouter une voie d'acheminement au site</h4>
-<?= $this->Form->create() ?>
-<?= $this->Form->input('SiteName', array('label'=>'Nom du site à raccoder','options' => $tabSitesTries))?>
-<?= $this->Form->input('max_capacity', array('label'=>'Capacité maximum','type'=>'number'))?>
-<?= $this->Form->input('name', array('label'=>'Nom de la voie'))?>
-<?= $this->Form->submit('Ajouter la voie',array('name' => 'submit')) ?>
-<?= $this->Form->end() ?>
+    <?= $this->Form->create() ?>
+    <?= $this->Form->input('SiteName', array('label' => 'Nom du site à raccoder', 'options' => $tabSitesTries)) ?>
+    <?= $this->Form->input('max_capacity', array('label' => 'Capacité maximum', 'type' => 'number')) ?>
+    <?= $this->Form->input('name', array('label' => 'Nom de la voie')) ?>
+    <?= $this->Form->submit('Ajouter la voie', array('name' => 'submit')) ?>
+    <?= $this->Form->end() ?>
 </section>
 
 
 <!--Formulaire d'edition à cacher et à dévoiler avec javascript-->
 <section class="large-6">
     <h4>Editer le site</h4>
-<?= $this->Form->create($new) ?>
-<?= $this->Form->input('name',array('label'=>'Nom')) ?>
-<?= $this->Form->input('type',array('options' => array(
-    'consumer' => 'consumer',
-    'producer' => 'producer')))?>
-<?= $this->Form->input('location_x',array('label'=>'Latitude','type'=>'number')) ?>
-<?= $this->Form->input('location_y',array('label'=>'Longitude','type'=>'number')) ?>
-<?= $this->Form->input('stock',['type'=>'number']) ?>
-<?= $this->Form->submit('Editer',array('label'=>'Editer','name' => 'submit')) ?>
+    <?= $this->Form->create($new) ?>
+    <?= $this->Form->input('name', array('label' => 'Nom')) ?>
+    <?=
+    $this->Form->input('type', array('options' => array(
+            'consumer' => 'consumer',
+            'producer' => 'producer')))
+    ?>
+    <?= $this->Form->input('location_x', array('label' => 'Latitude', 'type' => 'number')) ?>
+    <?= $this->Form->input('location_y', array('label' => 'Longitude', 'type' => 'number')) ?>
+    <?= $this->Form->input('stock', ['type' => 'number']) ?>
+<?= $this->Form->submit('Editer', array('label' => 'Editer', 'name' => 'submit')) ?>
 <?= $this->Form->end() ?>
 </section>
 
